ec6ef840eecff30ce9f205674a6638b7d2bb2a06
Nick Doiron <ndoiron@mapmeld.com>
2017-08-11 15:52:24 -0400

    use different library

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+//var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+const compression = require('compression');
 const mongoose = require('mongoose');
-const District = require('./models/district');
 mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGODB_URI || 'localhost:27017/nodetest1');
 
 // New Code
@@ -22,61 +22,14 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(compression);
 app.use('/', routes);
-
-app.get('/voteson/:district_name', (req, res) => {
-  District.findOne({ filename: req.params.district_name }, (err, district) => {
-    if (err) {
-      return res.json(err);
-    }
-    if (district) {
-      res.json([district.hot, district.not]);
-    } else {
-      res.json([0, 0]);
-    }
-  });
-});
-
-app.get('/set/:district_name', (req, res) => {
-  District.findOne({ filename: req.params.district_name }, (err, district) => {
-    if (err) {
-      return res.json(err);
-    }
-    if (district) {
-      if (req.query.hot) {
-        district.hot++;
-        district.save((err) => {
-          if (err) {
-            return res.json(err);
-          }
-        });
-      }
-      if (req.query.not) {
-        district.not++;
-        district.save((err) => {
-          if (err) {
-            return res.json(err);
-          }
-        });
-      }
-    } else {
-      var d = new District({
-        hot: ((req.query.hot * 1) || 0),
-        not: ((req.query.not * 1) || 0),
-        filename: req.params.district_name
-      });
-      d.save((err) => {
-        return res.json({ success: 'true' });
-      });
-    }
-  });
-});
 
 app.use('/users', users);
 
--- public/js/code.js
+++ public/js/code.js
@@ -12,10 +12,9 @@ function loadRandomFromList() {
     show_image('/images/' + selected, 200, 200, "Google");
     document.getElementById("hotClicker").disabled = false;
     document.getElementById("notClicker").disabled = false;
-    fetch("/voteson/" + selected)
-      .then(function(response) {
-        return response.json();
-      }).then(function(json) {
+    $.get("/voteson/" + selected, function(b) {
+      var json = JSON.parse(b);
+      console.log(json);
         document.getElementById("hotClicks").innerHTML = json[0];
         document.getElementById("notClicks").innerHTML = json[1];
       });
@@ -33,24 +32,14 @@ function onHotClick() {
   document.getElementById("hotClicker").disabled = true;
   document.getElementById("notClicker").disabled = true;
     document.getElementById("hotClicks").innerHTML = (document.getElementById("hotClicks").innerHTML * 1 + 1);
-    fetch("/set/" + selected + "?hot=1")
-      .then(function(response) {
-        return response.json();
-      }).then(function(json) {
-        //console.log(json);
-      });
+    $.get("/set/" + selected + "?hot=1", function() { });
     setTimeout(loadRandomFromList, 350);
 };
 function onNotClick() {
     document.getElementById("hotClicker").disabled = true;
     document.getElementById("notClicker").disabled = true;
     document.getElementById("notClicks").innerHTML = (document.getElementById("notClicks").innerHTML * 1 + 1);
-    fetch("/set/" + selected + "?not=1")
-      .then(function(response) {
-        return response.json();
-      }).then(function(json) {
-        //console.log(json);
-      });
+    $.get("/set/" + selected + "?not=1", function() { });
     setTimeout(loadRandomFromList, 350);
 };
 
