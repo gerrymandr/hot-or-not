83de09d72ae8d97bd029fca745f74e9d4de31d9b
removedporn <86824510+removedporn@users.noreply.github.com>
2022-05-11 23:37:29 +0800

    Delete models directory

--- models/district.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const districtSchema = mongoose.Schema({
-  filename: String,
-  hot: Number,
-  not: Number
-});
-
-module.exports = mongoose.model('District', districtSchema);
